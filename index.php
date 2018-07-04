2c7efcf21ece04a4db6c1588e0d3c1a74cf93582
salis77 <40762075+salis77@users.noreply.github.com>
2018-07-03 19:16:42 -1200

    attaching style.css file to the index.php

--- index.php
+++ index.php
@@ -1,67 +1,8 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<style type="text/css">
-			a{
-			text-decoration: none; 
-			color: #0000ff57;	
-			}
-			a:hover{
-				color:blue;
-				transition: all ease-in-out 1s;
-			}
-		.result{
-			display: flex;
-			justify-content: center;
-			align-items: center;
-			background: white;
-			border-radius: 5px;
-			width:500px;
-			height: auto;
-			margin: 0 auto;
-			margin-top: 5px;
-			border":2px solid black;
-		}
-		body{
-  		background: linear-gradient(-90deg, red, yellow);
-  			font-family:'BYEKAN';
-
-		}
-		.info{
-			border: 1px solid black;
-			border-radius: 5px;
-			padding: 5px;
-			width:500px;
-			margin-top: 100px;
-		}
-		.send{
-			border: 1px solid black;
-			border-radius: 5px;
-			padding: 4.5px;
-			width:200px;
-			margin-top: 100px;
-
-		}
-		.uclearn-logo{
-			  display: flex;
-  			justify-content: center;
-  			align-items: center;
-  			margin: 0 auto;
-		}
-		.title{
-			 display: flex;
-  			justify-content: center;
-  			align-items: center;
-  			margin: 0 auto;
-		}
-		@font-face{font-family:'BYekan';src: url('fonts/BYekan.eot?#') format('eot'),url('fonts/BYekan.woff') format('woff'),url('fonts/BYekan.ttf') format('truetype');}
-
-		input{
-				font-family:'BYEKAN';
-
-		}
-	</style>
 	<title>ajax with php & jquery</title>
+	<link rel="stylesheet" src="styles/style.css" >
 	<script src="jquery.js" type="text/javascript"></script>
 	<script type="text/javascript">
 		$(document).ready(function(){
